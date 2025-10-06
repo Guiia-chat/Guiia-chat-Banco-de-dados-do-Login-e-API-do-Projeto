cede0f2994263b8ef41c5845814f46a7eaafa7f1
Davi-dev1 <davigancalveesdasilva@gmail.com>
2025-10-06 16:15:24 -0300

    Enviando primeiros arquivos do projeto
    Signed-off-by: Davi-dev1 <davigancalveesdasilva@gmail.com>

---
+++ LoginIA/LoginIA/src/main/java/br/com/loginGui/LoginIA/LoginIaApplication.java
@@ -0,0 +1,13 @@
+package br.com.loginGui.LoginIA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginIaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginIaApplication.class, args);
+	}
+
+}
---
+++ LoginIA/LoginIA/src/test/java/br/com/loginGui/LoginIA/LoginIaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.loginGui.LoginIA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginIaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
